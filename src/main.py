c143a14cc3cecd23ff733e4a5ebf6a481aa57d17
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2025-12-29 16:48:10 +0000

    implemented text normalizer

--- src/main.py
+++ src/main.py
@@ -1,17 +1,32 @@
 # entry point
-
+# OCR engine 
 from pathlib import Path
 from ocr.ocr_engine import extract_text
+# Text normalizer 
+from utils.text_normalizer import normalize_text
+
 
 def main():
     print("PAN & Adhaar Reader Started")
+    # access the root path 
     BASE_DIR = Path(__file__).resolve().parent.parent
-    print(BASE_DIR)
+    # access the file path 
     image_path = BASE_DIR / "data/samples/pan/pan1.jpg"
+    # priting the path 
     print(image_path)
-    text = extract_text(image_path)
+    # calling the function to get the details of pan 
+    raw_text = extract_text(image_path)
     print("----RAW OCR OUTPUT----")
-    print(text)
-
+    # clean text 
+    clean_text = normalize_text(raw_text)
+    print(clean_text)
+      
 if __name__ == "__main__":
     main()
+
+
+# notes!
+# Important things :
+# OCR gives raw data 
+# Normalizer makes it usable 
+# We never extract fields from raw OCR
--- src/utils/text_normalizer.py
+++ src/utils/text_normalizer.py
@@ -1,3 +1,14 @@
 # uppercase 
 # Fix OCR mistakes (o-0m i-1)
 # removal extra space 
+
+def normalize_text(text:str) -> str:
+    """
+    Cleans OCR Text
+    """
+    text = text.upper()
+    text = text.replace("\n\n","\n")
+    text= text.strip()
+
+    return text
+
