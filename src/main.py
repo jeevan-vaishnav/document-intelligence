fb5e690ce25f40519d2c19c307a4f91dd78d9c8f
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2025-12-29 14:36:51 +0000

    ocr engine implemented

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
-# entry point 
+# entry point
+
+from pathlib import Path
+from ocr.ocr_engine import extract_text
+
 def main():
     print("PAN & Adhaar Reader Started")
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    print(BASE_DIR)
+    image_path = BASE_DIR / "data/samples/pan/pan1.jpg"
+    print(image_path)
+    text = extract_text(image_path)
+    print("----RAW OCR OUTPUT----")
+    print(text)
 
 if __name__ == "__main__":
     main()
--- src/ocr/ocr_engine.py
+++ src/ocr/ocr_engine.py
@@ -1,3 +1,25 @@
 # image path 
 # output raw
 # noparsing logic here
+
+# import bring libary
+# def extrat_text -> function 
+# image_path:str -> type hint (optional, just for clarity)
+# Image.open() -< load image 
+# image_to_string() -> OCR
+#  return text -> give result back
+
+import pytesseract
+from PIL import Image
+
+def extract_text(image_path:str) -> str:
+    """
+    Takes image path and returns raw OCR text
+    """
+    image = Image.open(image_path)
+    text = pytesseract.image_to_string(image)
+    return text
+
+
+
+
